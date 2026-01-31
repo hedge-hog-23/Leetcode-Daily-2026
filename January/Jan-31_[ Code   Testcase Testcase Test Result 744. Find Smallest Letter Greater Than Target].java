1552700dfb9a98a5aecd742ac9d6b2efbb11ef72
SENTHIL <123928930+hedge-hog-23@users.noreply.github.com>
2026-01-31 17:11:46 +0530

    Update Jan-31_[ Code   Testcase Testcase Test Result 744. Find Smallest Letter Greater Than Target].java

--- January/Jan-31_[ Code   Testcase Testcase Test Result 744. Find Smallest Letter Greater Than Target].java
+++ January/Jan-31_[ Code   Testcase Testcase Test Result 744. Find Smallest Letter Greater Than Target].java
@@ -19,3 +19,15 @@ class Solution {
         return letters[idx];
     }
 }
+
+
+//linear search 
+// class Solution {
+//     public char nextGreatestLetter(char[] letters, char target) {
+//         for(char c : letters){
+//             if(c > target)
+//                 return c;
+//         }
+//         return letters[0];
+//     }
+// }
